a967ee6952261e6cf1a0c24524f8ec3ce51bad9c
Stephen Anderson <stephenganderson10@gmail.com>
2021-01-28 13:32:06 -0600

    importing all homework files into folder

---
+++ Instructions/starter_code/api_keys.py
@@ -0,0 +1,7 @@
+# OpenWeatherMap API Key
+weather_api_key = "92bec3fd4dafd9a34ef219cf9b95701f"
+
+
+#Get from google cloud account
+# Google API Key
+g_key = "AIzaSyDArgH84buqakMIQTE6FqBhSeoPPqnFqZ0"
