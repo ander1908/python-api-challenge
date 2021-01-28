f8b1c5a8b7295706501c308196a48fc171cb0258
ander1908 <73594110+ander1908@users.noreply.github.com>
2021-01-28 13:37:10 -0600

    Delete api_keys.py
    Keys removal

--- Instructions/starter_code/api_keys.py
+++
@@ -1,7 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "92bec3fd4dafd9a34ef219cf9b95701f"
-
-
-#Get from google cloud account
-# Google API Key
-g_key = "AIzaSyDArgH84buqakMIQTE6FqBhSeoPPqnFqZ0"
